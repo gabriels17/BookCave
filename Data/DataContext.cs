82c6f6c33b85c199516b791af66fb7ba5ff309d1
gabriels17 <gabriel.sighvatsson@gmail.com>
2018-05-03 12:08:17 +0000

    Added the database connection

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using BookCave.Data.EntityModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCave.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H17;Persist Security Info=False;User ID=VLN2_2018_H17_usr;Password=freeLeop@rd10;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+        }
+    }
+}
