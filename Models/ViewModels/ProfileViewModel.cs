d2ee9dcba224eafe2724aa86783de4b206b1a24f
Olaf97 <olafurd1997@gmail.com>
2018-05-09 14:06:14 +0000

    User can now edir profile

--- Models/ViewModels/ProfileViewModel.cs
+++ Models/ViewModels/ProfileViewModel.cs
@@ -2,7 +2,10 @@ namespace BookCave.Models.ViewModels
 {
     public class ProfileViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteBook { get; set; }
         public string Email { get; set; }
-        public string Name { get; set; }
+        public string Image { get; set; }
     }
 }
