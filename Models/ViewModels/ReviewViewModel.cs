2fb23bf1807a7b1fead3a0198fc533c8422dba79
GartinMarrix <baldvinbw0@gmail.com>
2018-05-10 12:15:26 +0000

    Added review option

---
+++ Models/ViewModels/ReviewViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BookCave.Models.ViewModels
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+        public string UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
